2f2a033401e88867d09ff1115c4931775e309889
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-03 21:24:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-#def change_my_name
-#  name = "Frank"
- # puts name.reverse
-  #return name
-#end
-
 
 def parrot (phrase = "Squawk!")
   phrase = "Squawk!"
